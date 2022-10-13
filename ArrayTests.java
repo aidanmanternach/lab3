01347c313279b01ce9a6909723bf929cc43f21e4
aidanmanternach <92654166+aidanmanternach@users.noreply.github.com>
2022-10-13 08:33:55 -0700

    fixed bugs in reverse methods

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,17 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverseInPlaceMine() {
+    int[] input1 = {1, 2};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[] {2, 1}, input1);
+  }
+
+  @Test
+  public void testReversedMine() {
+    int[] input1 = {1, 2};
+    assertArrayEquals(new int[]{2, 1}, ArrayExamples.reversed(input1));
+  }
 }
